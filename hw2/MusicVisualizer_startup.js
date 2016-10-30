d4e945a97c92a9221b4f55b68f06db9174962c4a
riccchard <kshtrue95@kaist.ac.kr>
2016-10-30 12:58:58 +0900

    hw2

--- hw2/MusicVisualizer_startup.js
+++ hw2/MusicVisualizer_startup.js
@@ -18,7 +18,7 @@ var HEIGHT = 480;
 var SOUND_METER_GAP = 10;
 var SOUND_METER_WIDTH = 60;
 var SOUND_METER_HEIGHT = HEIGHT;
-var SOUND_METER_MIN_LEVEL = -96.0;  // dB
+var SOUND_METER_MIN_LEVEL = -72.0;  // dB
 
 var micOn = false;
 var filePlayOn = false;
@@ -26,11 +26,21 @@ var filePlayOn = false;
 var animation_function;
 var animation_id;
 
-var prev_band_level = new Array(10); 
-for (var i=0; i <10;i++ ) {
-	prev_band_level[i] = 0;		
+var prev_band_level = new Array(6); 
+for (var i=0; i <10; i++ ) {
+	prev_band_level[i] = 0;
+}
+var R=240;
+
+var prev_r = new Array(6); 
+for (var i=0; i <6; i++ ) {
+	prev_r[i] = 0;
 }
 
+var k=1; // window size
+var a = 0; // hue value
+var theta = 0; // theta value
+var TOTAL_MIN = SOUND_METER_MIN_LEVEL + Math.log10(6);
 
 // load demo audio feils
 var demo_buffer;
@@ -57,7 +67,6 @@ window.onload=function(){
 	vis_value = document.getElementById("loudnessValue");
 	vis_view.width =  WIDTH;
 	vis_view.height = HEIGHT;
-
 	
 	// create audio context
 	context = new AudioContext();
@@ -68,7 +77,7 @@ window.onload=function(){
     analyser.smoothingTimeConstant = 0;		
 
 	var demoReq = new XMLHttpRequest();
-    demoReq.open("Get","demo1.mp3",true);
+    demoReq.open("Get","SKRILLEX.mp3",true);
     demoReq.responseType = "arraybuffer";
     demoReq.onload = function(){
         context.decodeAudioData(demoReq.response, function(buffer){demo_buffer = buffer;});
@@ -92,9 +101,9 @@ function setAnimationFunction (mode_num) {
     	animation_id = setInterval(animation_function, context.sampleRate/analyser.fftSize);
 	}
 }
-
+var loudness = SOUND_METER_MIN_LEVEL;
+var pre = SOUND_METER_MIN_LEVEL;
 function draw_octaveband() {
-
 	// get samples 
 	var data_array = new Float32Array(analyser.frequencyBinCount);
 	analyser.getFloatFrequencyData(data_array);
@@ -103,12 +112,27 @@ function draw_octaveband() {
 
 
 	// display the loudness value (this is for verifying if the level is correctly computed.)
-	var loudness = octaveband_level_db[0];
+	
+	//if ( (octaveband_level_db[4] > -25) && (pre + 3<octaveband_level_db[4]) ){
+	//	loudness = octaveband_level_db[4];
+	//}
+	//pre = octaveband_level_db[4]
+	//if (octaveband_level_db[2] >-25){
+	//	loudness = octaveband_level_db[2];
+	//}
+	loudness = octaveband_level_db[0];
 	vis_value.innerHTML = '32Hz-Band Level (dB): ' + loudness + ' dB'
 
 	// 2d canvas context
 	var drawContext = vis_view.getContext('2d');
-	
+
+	// canvas size
+	if (k == 0){
+		console.log('H = 480')
+		HEIGHT = 480;
+		vis_view.height = 480;
+		k = 1;
+	}
 	// fill rectangular (for the entire canvas)
 	drawContext.fillStyle = 'rgb(0,0,0)';
 	drawContext.fillRect(0, 0, WIDTH, HEIGHT);
@@ -116,15 +140,14 @@ function draw_octaveband() {
 	for (var i=0; i<10; i++) {
 
 		// fill rectangular (for the sound level)
-		var sound_level = (octaveband_level_db[i]-SOUND_METER_MIN_LEVEL)/(12-SOUND_METER_MIN_LEVEL)*SOUND_METER_HEIGHT;
+		var sound_level = (octaveband_level_db[i]-SOUND_METER_MIN_LEVEL)/(0-SOUND_METER_MIN_LEVEL)*SOUND_METER_HEIGHT;
 		var sound_level_env;
-
 
 		///// asymmetric envelope detector
 		// fill out here with your code
 		// note that you can use "prev_band_level" array defined above to store the decaying level
 		
-		if (prev_band_level[i] < Math.pow(10,sound_level/10)){
+		if (prev_band_level[i] < sound_level){
 			sound_level_env = sound_level;
 		}
 
@@ -139,7 +162,7 @@ function draw_octaveband() {
 		drawContext.rect(x, SOUND_METER_HEIGHT, SOUND_METER_WIDTH, -sound_level_env);
 
 		// color
-		var hue = Math.floor(255/9*i);
+		var hue = Math.floor(255/10*i);
 		var saturation = 255;
 		var value = 255;
 		var rgb = hsvToRgb(hue, saturation, value);
@@ -150,12 +173,95 @@ function draw_octaveband() {
 }
 
 function draw_MyOwn() {
-	//
-	//
-	// fill out here with your code
-	// 
-	// 
-	//
+
+
+	// get samples 
+	var data_array = new Float32Array(analyser.frequencyBinCount);
+	analyser.getFloatFrequencyData(data_array);
+
+	var octaveband_level_db = calc_octaveband_my_own(data_array)
+
+	// display the loudness value (this is for verifying if the level is correctly computed.)
+	loudness = octaveband_level_db[0];
+	vis_value.innerHTML = '32Hz-Band Level (dB): ' + loudness + ' dB';
+
+	// color
+	if ( (data_array[4] > -25) && (pre + 3<data_array[4]) ){
+		a = a + 50;
+		a = a % 360;
+		//loudness = data_array[4];
+	}
+	pre = data_array[4];
+
+	// ratation
+	if (data_array[2]>-25){
+		theta = theta + Math.PI/300;
+		//loudness = data_array[3];
+	}
+
+	// 2d canvas context
+	var drawContext = vis_view.getContext('2d');
+
+	// canvas size
+	if (k == 1){
+		console.log('H = 720')
+		HEIGHT = 720;
+		vis_view.height = 720;
+		k = 0;
+	}
+
+	// fill rectangular (for the entire canvas)
+	//drawContext.fillStyle = 'rgb(0,0,0)';
+	//drawContext.fillRect(0, 0, WIDTH, HEIGHT);
+	drawContext.fillStyle = 'rgb(255,255,255)';
+	drawContext.fillRect(2, 2, WIDTH-4,HEIGHT-4);
+
+	// total value
+	var total_value = 0;
+	for (var j=0; j<6; j++) {
+		total_value = total_value + Math.pow(10,octaveband_level_db[j]/10);
+	}
+	total_value = 10.0*Math.log10(total_value);
+
+	// alpha value for png image
+	var alpha = (total_value-TOTAL_MIN)/(50-TOTAL_MIN)*1
+	//drawContext.globalAlpha = alpha;
+
+	//draw r
+	for (var i=0; i<6; i++) {
+		// fill samll circle (for the sound level)
+		var r = (octaveband_level_db[i]-SOUND_METER_MIN_LEVEL)/(-10-SOUND_METER_MIN_LEVEL)*(R/2);
+		var r_env;
+
+		///// asymmetric envelope detector
+		// fill out here with your code
+		// note that you can use "prev_band_level" array defined above to store the decaying level
+		if (prev_r[i] < r){
+			r_env = r;
+		}
+		else{
+			r_env = prev_r[i] - R/256
+		}
+		prev_r[i] = r_env;
+		//console.log(r_env);
+
+		if (r_env < 0){
+			r_env = 0;
+		}
+		// shape
+		drawContext.beginPath();
+		var x = (WIDTH/2) + R*Math.cos(Math.PI/2 + Math.PI/3*i + theta)
+		var y = (HEIGHT/2) - R*Math.sin(Math.PI/2 + Math.PI/3*i + theta)
+		drawContext.arc(x, y, r_env, 0, Math.PI*2, true);
+
+		// color
+		var hue = a;
+		var saturation = 255;
+		var value = 255;
+		var rgb = hsvToRgb(hue, saturation, value);
+		drawContext.fillStyle='rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'; 
+		drawContext.fill();
+	}
 }
 
 function playMic()
@@ -218,6 +324,10 @@ function playFile() {
 
     sourceNode.buffer = demo_buffer;
     sourceNode.connect(context.destination);
+
+    // music end
+    sourceNode.onended = turnOffFileAudio;
+
     sourceNode.start(0);
 
 	sourceNode.connect(analyser);
@@ -244,8 +354,10 @@ function turnOffMicAudio() {
 function turnOffFileAudio() {
 	var demoAudio = document.getElementById("demoAudio");
 	demoAudio.innerHTML = 'Demo Audio Play'
-	sourceNode.stop(0);
-    sourceNode = null;
+	if (sourceNode != null){
+		sourceNode.stop(0);
+	    sourceNode = null;
+	}
     filePlayOn = false;
 
 	stopAnimation();
--- hw2/octave_band_my_own.js
+++ hw2/octave_band_my_own.js
@@ -1,20 +1,19 @@
-function calc_octaveband(input_array) {
-	var lower_freqs = [22, 44, 88, 177, 355, 710, 1420, 2840, 5680, 11360];
-	var upper_freqs = [44, 88, 177, 355, 710, 1420, 2840, 5680, 11360, 22720];
-	var center_freqs = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
+function calc_octaveband_my_own(input_array) {
+	var lower_freqs = [22,44,88,355,1420,3550];
+	var upper_freqs = [44,88,355,1420,3550,22720];
+	//var center_freqs = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
 
 	// compute FFT power (linear scale) 
 	var fft_power = new Array(input_array.length);
-
 	for (var i = 0; i < input_array.length; i++) {
 		fft_power[i] = Math.pow(10.0, input_array[i]/10.0);
 	}
 
-	var band_power = new Array(center_freqs.length);
+	var band_power = new Array(6);
 
 
-	for (var i = 0; i < center_freqs.length; i++) {
-		band_power[i]=0;
+	for (var i = 0; i < 6; i++) {
+		band_power[i] = 0;
 		for (var j = Math.floor(lower_freqs[i]/22.1875); j < Math.floor(upper_freqs[i]/22.1875); j++){
 			band_power[i] = band_power[i] + fft_power[j];
 		}
